2c4395ccac7552cf6e6ef61b28254f29d5eee202
Chad Sahlhoff <csahlhoff@gmail.com>
2013-01-18 19:18:18 -0800

    events ajax is working!!

--- public/scripts/left.js
+++ public/scripts/left.js
@@ -2,30 +2,6 @@
 window.onload = function(){    
 
 
-	var events =function() {
-		console.log('in events')
-	   $.get('/superdash/events', function (data) {
-		
-		if (res != '') {
-		    var output = '<ul id="listname" data-inset=true>';
-		    $.each(res, function (i, Object) {
-		       output += '<li>' + Object.reg + '</li>';
-		     });
-		    output += '</ul>';
-
-		    console.log(output);
-
-		    $("#myObject").append(output).trigger("create");
-		    $("#myObject").listview();
-		    $("#myObject").listview('refresh');
-		  }
-	      
-	    });
-
-	 //setTimeout(events, 5000);
-
-	}
-
 
 	var heatmap =function() {
 		console.log('in heatmap')
@@ -58,11 +34,11 @@ window.onload = function(){
 	      console.log(data);
 	    });
 
-	 //setTimeout(official, 50000000);
+	 setTimeout(official, 5000);
 
 	}
 
 
-	setTimeout(events, 5000);
+	setTimeout(official, 5000);
 
 };
