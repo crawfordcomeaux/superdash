18b9a2038ca42ecbea453f8ad2e8a24a225a86da
Crawford Comeaux <crawford.comeaux@gmail.com>
2013-01-23 21:29:23 -0600

    Added "enabled" config key to stream, changed mongodb address to mongo on EC2

--- config/development.js
+++ config/development.js
@@ -1,20 +1,24 @@
 exports.loggerFormat = 'dev';
 exports.useErrorHandler = true;
-exports.mongodb = 'mongodb://superdash:sup3rdashm3@ds047447.mongolab.com:47447/heroku_app11073055';
+exports.mongodb = 'mongodb://ec2-50-19-142-220.compute-1.amazonaws.com/superdash';
 exports.sessionSecret = 'i love making code really fast';
 
 //exports.enableGuestLogin = true;
 //exports.enableEmailLogin = true;
 exports.twitter = {
   consumerKey: 'Ak2dsjQhvYtsiJ343Q',
-  consumerSecret: 'iuh6rHrlfY7qDnhWMctDCuR4J67N1vOOVq3jL5YVQ'
+  consumerSecret: 'iuh6rHrlfY7qDnhWMctDCuR4J67N1vOOVq3jL5YVQ',
+  enabled: false
 };
 exports.facebook = {
   clientID: '311801745605588',
   clientSecret: 'cadebf6e81ac21553f76631ed5d50f94',
-  callbackURL: 'http://127.0.0.1:3000/auth/facebook/callback'
+  callbackURL: 'http://127.0.0.1:3000/auth/facebook/callback',
+  enabled: false
 };
 
 exports.instagram = {
   clientID: 'f2604eb9377e4b89844d8e743ee930fc',
-  clientSecret: 'b720d1c2fc9e4bf2854140d2453af1cd'}
+  clientSecret: 'b720d1c2fc9e4bf2854140d2453af1cd',
+  enabled: false
+}
--- config/production.js
+++ config/production.js
@@ -7,10 +7,18 @@ exports.sessionSecret = process.env.SESSION_SECRET || 'i love making code really
 //exports.enableEmailLogin = true;
 exports.twitter = {
   consumerKey: process.env.TWITTER_KEY,
-  consumerSecret: process.env.TWITTER_SECRET
+  consumerSecret: process.env.TWITTER_SECRET,
+  enabled: true
 };
 exports.facebook = {
   clientID: process.env.FB_ID,
   clientSecret: process.env.FB_SECRET,
-  callbackURL: process.env.FB_CALLBACK || 'http://superdash.superdash.jit.su/auth/facebook/callback'
+  callbackURL: process.env.FB_CALLBACK || 'http://superdash.superdash.jit.su/auth/facebook/callback',
+  enabled: true
 };
+exports.instagram = {
+  clientID: 'f2604eb9377e4b89844d8e743ee930fc',
+  clientSecret: 'b720d1c2fc9e4bf2854140d2453af1cd',
+  enabled: true
+}
+
