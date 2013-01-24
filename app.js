34d2ce5356091cd01b2e70274dbcf888b977b7ad
rump <git@seandunaway.com>
2013-01-24 04:57:43 -0800

    add jade browser

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var config = require('./config');
 var routes = require('./routes');
 var express = require('express');
+var jadeBrowser = require('jade-browser');
+var mongoose = require('mongoose');
 var http = require('http');
 var twitter = require('./lib/twitter');
 
@@ -12,7 +14,8 @@ app.use(express.bodyParser());
 app.use(express.errorHandler());
 app.use(express.favicon());
 app.use(express.logger());
-app.use(express['static']('./public'));
+app.use('/img', express['static']('./public/img'));
+app.use(require('connect-assets')({ src: './public' }));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -33,9 +36,20 @@ app.all('/superdash/*', function (req, res) {
   res.json(res.jsonData);
 });
 
+// Export jade templates to reuse on client side
+// This includes a kind of terrible cache-buster hack
+// It generates a new cache-busting query string for the script tag every time the server starts
+// This should probably only happen every time there's a change to the templates.js file
+var jadeTemplatesPath = '/js/templates.js';
+app.use(jadeBrowser(jadeTemplatesPath, ['*.jade', '*/*.jade'], { root: __dirname + '/views', minify: true }));
+var jadeTemplatesCacheBuster = (new Date()).getTime();
+var jadeTemplatesSrc = jadeTemplatesPath + '?' + jadeTemplatesCacheBuster;
+global.jadeTemplates = function() { return '<script src="' + jadeTemplatesSrc + '" type="text/javascript"></script>'; };
+
 
 app.listen(config.port, function(){
   console.log("Express server listening on port " + config.port);
 });
 
 twitter.listen(http.createServer(app));
+mongoose.connect(config.mongodb);
--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   'port': 3000,
   'pwd': process.cwd(),
   'root': '/',
-  'mongodb': 'mongodb://' + process.env.NODE_MONGODB || 'ec2-50-19-142-220.compute-1.amazonaws.com/superdash',
+  'mongodb': process.env.NODE_MONGODB || 'mongodb://ec2-50-19-142-220.compute-1.amazonaws.com/superdash',
   'sessionSecret': 'i love making code really fast',
   'title': 'SuperDash - A Super Dashboard for a Super Bowl',
   'twitter_ids': [
